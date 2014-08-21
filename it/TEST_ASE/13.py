71ba08ad24c5c8adfc3b830c733a284112dafa0a
itamblyn <Isaac Tamblyn>
2014-08-21 17:34:28 -0400

    added ref to HH.xyz

--- it/TEST_ASE/13.py
+++ it/TEST_ASE/13.py
@@ -3,17 +3,16 @@ from ase.optimize import QuasiNewton
 from ase.io import write, read
 from ase.constraints import FixAtoms, Hookean
 
-atoms = read('initial.xyz')
-atoms.set_calculator(Dftb(label='h2o',
+atoms = read('HH.xyz')
+atoms.set_calculator(Dftb(label='hh',
                          atoms=atoms,
                          Hamiltonian_MaxAngularMomentum_='',
-                         Hamiltonian_MaxAngularMomentum_O='"p"',
                          Hamiltonian_MaxAngularMomentum_H='"s"',
                          ))
 
-c = Hookean(a1=1, a2=2, rt=0.1, k=50.)
+c = Hookean(a1=0, a2=1, rt=0.1, k=50.)
 atoms.set_constraint(c)
 dyn = QuasiNewton(atoms, trajectory='atoms.traj')
 dyn.run(steps=10)
-print atoms.get_distance(a0=1,a1=2)
+print atoms.get_distance(a0=0,a1=1)
 write('test.final.xyz', atoms)
