01796f48ff7dac64a8fc2637d7d3bf977e500339
Amber Maharaj <amber.maharaj@uoit.net>
2014-08-26 14:09:31 -0400

    Added a constraint class "Hookean Always"

--- it/TEST_ASE/13.py
+++ it/TEST_ASE/13.py
@@ -1,7 +1,11 @@
 from ase.calculators.dftb import Dftb
 from ase.optimize import QuasiNewton
 from ase.io import write, read
-from ase.constraints import FixAtoms, Hookean
+from ase.constraints import FixAtoms, Hookean_Always
+
+import numpy as np
+
+text_file = open("Output.txt", "w")
 
 atoms = read('HH.xyz')
 atoms.set_calculator(Dftb(label='hh',
@@ -10,9 +14,29 @@ atoms.set_calculator(Dftb(label='hh',
                          Hamiltonian_MaxAngularMomentum_H='"s"',
                          ))
 
-c = Hookean(a1=0, a2=1, rt=0.1, k=50.)
+c = Hookean_Always(a1=0, a2=1, k=0, rt=0.6)
 atoms.set_constraint(c)
+OP = []
+
+def print_distance(a=atoms):
+        distance = a.get_distance(a0=0,a1=1)
+	OP.append(distance)
+        epot = a.get_potential_energy() / len(a)
+        ekin = a.get_kinetic_energy() / len(a)
+        ETOT = epot+ekin
+        OP.append(ETOT)
+
 dyn = QuasiNewton(atoms, trajectory='atoms.traj')
+dyn.attach(print_distance)
 dyn.run(steps=10)
-print atoms.get_distance(a0=0,a1=1)
+print atoms.get_distance(a0=0, a1=1)
 write('test.final.xyz', atoms)
+
+OP_f = [OP[i:i+2] for i in xrange(0,len(OP),2)]
+OP_f_text = np.savetxt('out.tmp', OP_f)         # reads array and saves into a file as a string
+OP_f_infile = open('out.tmp','r')       # open file to write string array onto
+OP_f_text = OP_f_infile.read()
+
+text_file.write(OP_f_text)
+text_file.close()
+                     
